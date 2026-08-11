e7cedd0b1fba86cfd443e4df57e7dcc708d916c9
Kartik Gupta <kg877253@gmail.com>
2026-08-11 22:23:29 +0530

    Time: 161 ms (82.87%) | Memory: 12.7 MB (64.61%) - LeetSync

--- 204-count-primes/count-primes.cpp
+++ 204-count-primes/count-primes.cpp
@@ -6,14 +6,19 @@ public:
         vector<bool> prime(n, true);
         prime[0] = prime[1] = false;
 
+        int count = n - 2;
+
         for (int i = 2; i * i < n; i++) {
             if (prime[i]) {
                 for (int j = i * i; j < n; j += i) {
-                    prime[j] = false;
+                    if (prime[j]) {
+                        prime[j] = false;
+                        count--;
+                    }
                 }
             }
         }
 
-        return count(prime.begin(), prime.end(), true);
+        return count;
     }
 };
